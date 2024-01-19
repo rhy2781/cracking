d5af793944bb7e9f9f064bd307f83ad3a51d5687
Robert Yamasaki <rhy2781@rit.edu>
2024-01-18 16:50:00 -0800

    stringCompression

---
+++ src/arraysAndStrings/stringCompression/Solution.java
@@ -0,0 +1,10 @@
+package arraysAndStrings.stringCompression;
+
+public class Solution {
+	public static void main(String[] args){
+		pointerApproach pa = new pointerApproach();
+		System.out.println(pa.stringCompression("aabcccccaaaa"));
+		System.out.println(pa.stringCompression("abcdefghijk"));
+
+	}
+}
---
+++ src/arraysAndStrings/stringCompression/Solution.py
@@ -0,0 +1,12 @@
+import pointerApproach
+
+
+def main():
+    pa = pointerApproach
+    print(pa.stringComparison("aabbbcccccaa"))
+    print(pa.stringComparison("abcdefg"))
+
+
+if __name__ =="__main__":
+    main()
+

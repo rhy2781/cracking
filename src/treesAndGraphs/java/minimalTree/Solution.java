6fdd3a38c3e60f8d73176cb7a00f4f7d403cfe7b
Robert Yamasaki <rhy2781@rit.edu>
2024-02-06 15:04:20 -0800

    moved static method into binary tree node class

--- src/treesAndGraphs/java/minimalTree/Solution.java
+++ src/treesAndGraphs/java/minimalTree/Solution.java
@@ -8,24 +8,10 @@ public class Solution {
 		minimalHeight m = new minimalHeight();
 		BinaryTreeNode root = m.createBinaryTreeNodeFromSortedArray(source);
 
-		printTreeLevel(root, 0);
+		BinaryTreeNode.printTreeLevel(root, 0);
 
 	}
 
-	public static void printTreeLevel(BinaryTreeNode root, int level) {
-		if (root != null) {
-			// Traverse the left subtree with an incremented level
-			printTreeLevel(root.left, level + 1);
 
-			// Print the current node's value with proper indentation
-			for (int i = 0; i < level; i++) {
-				System.out.print("\t");
-			}
-			System.out.println(root.value); // Use println to move to the next line after printing the value
-
-			// Traverse the right subtree with an incremented level
-			printTreeLevel(root.right, level + 1);
-		}
-	}
 
 }
